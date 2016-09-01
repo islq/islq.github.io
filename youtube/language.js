3441d24f32db93f6b9293bd491f6e7ef0df3dbdb
islq <islqislq@gmail.com>
2016-09-01 15:13:20 +0800

    Update language.js

--- youtube/language.js
+++ youtube/language.js
@@ -27,7 +27,7 @@ lang_sk =
 	tots:"Celkové výsledky: ",
 	language:"Jazyk",
 	search:"Vyhľadávaní",
-	seek:"Prehrať od",
+	seek:"Prehrat od",
 	menu:"Menu",
 	lang:
 		[
@@ -47,7 +47,7 @@ lang_cz =
 	tots:"Celkové výsledky: ",
 	language:"Jazyk",
 	search:"Vyhledávání",
-	seek:"Prehrať od",
+	seek:"Prehrat od",
 	menu:"Menu",
 	lang:
 		[
