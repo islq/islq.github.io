482046d9d75eea98de2a675ea6bd23b7f34d1a48
islq <islqislq@gmail.com>
2016-09-02 09:46:37 +0800

    add lang query params

--- youtube/language.js
+++ youtube/language.js
@@ -67,3 +67,21 @@ lang =
 	lang_sk,
 	lang_cz
 ]
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
+}
+var _query_lang = getQueryVariable('lang');
+if(typeof _query_lang != 'undefined') {
+    if (_query_lang>=0 && _query_lang <lang.length) {
+        lang_index = _query_lang;
+    }
+}
